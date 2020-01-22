5934198c445bf048ce6b19c0126983d3e52057ab
Nandra Saputra <nandra.saputras@gmail.com>
2020-01-22 19:48:22 +0700

    V 0.0.1
    + Setup Rainbow API SDK

--- app/src/main/java/com/nandra/myschool/MySchoolApp.kt
+++ app/src/main/java/com/nandra/myschool/MySchoolApp.kt
@@ -1,7 +1,15 @@
 package com.nandra.myschool
 
 import android.app.Application
+import com.ale.rainbowsdk.RainbowSdk
 
 class MySchoolApp : Application() {
 
+    private val appID = "8221b4c03a1611eab3730b8300261bf3"
+    private val appSecret = "cfUci19c0LrybD3mLqCYVTptY8YVoa0JMbEAsmLbw7iIdY3puLXVa2fWkTJR1WnM"
+
+    override fun onCreate() {
+        super.onCreate()
+        RainbowSdk.instance().initialize(this, appID, appSecret)
+    }
 }
